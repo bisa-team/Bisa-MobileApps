ef13a61add864150d3ec08429962d1fe3cdceec9
Ramadhan Oktovivian Muhammad <vianokto30@gmail.com>
2023-03-04 15:16:58 +0700

    update lupa password2

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -10,30 +10,51 @@ class LoginView extends GetView<LoginController> {
   const LoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Scaffold(
-          body: BackgroundTemplate(
-            widget: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 35),
-              children: [
-                const SizedBox(height: 150),
-                Image.asset("assets/images/logo.png"),
-                const SizedBox(height: 15),
-                const Text(
-                  "Masuk akun",
-                  style: TextStyle(
-                    fontSize: 20,
+    return SafeArea(
+      child: Scaffold(
+        body: BackgroundTemplate(
+          widget: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            children: [
+              const SizedBox(height: 150),
+              Image.asset("assets/images/logo.png"),
+              const SizedBox(height: 15),
+              const Text(
+                "Masuk akun",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.person),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(999),
                   ),
                 ),
-                const SizedBox(height: 15),
-                TextField(
+              ),
+              const SizedBox(height: 15),
+              Obx(
+                () => TextField(
+                  obscureText: controller.isHidden.value,
                   autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.done,
                   decoration: InputDecoration(
-                    labelText: "Email",
-                    prefixIcon: const Icon(Icons.person),
+                    labelText: "Password",
+                    prefixIcon: const Icon(Icons.key),
+                    suffixIcon: IconButton(
+                      onPressed: () => controller.isHidden.toggle(),
+                      icon: const Icon(Icons.remove_red_eye),
+                    ),
                     contentPadding: const EdgeInsets.symmetric(
                       horizontal: 15,
                     ),
@@ -42,81 +63,58 @@ class LoginView extends GetView<LoginController> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 15),
-                Obx(
-                  () => TextField(
-                    obscureText: controller.isHidden.value,
-                    autocorrect: false,
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      prefixIcon: const Icon(Icons.key),
-                      suffixIcon: IconButton(
-                        onPressed: () => controller.isHidden.toggle(),
-                        icon: const Icon(Icons.remove_red_eye),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(999),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Get.toNamed(Routes.LUPA_PASSWORD);
+                    },
+                    child: const Text(
+                      "Lupa password?",
+                      style: TextStyle(
+                        color: Color(0xff656260),
+                        decoration: TextDecoration.underline,
                       ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Get.toNamed(Routes.LUPA_PASSWORD);
-                      },
-                      child: const Text(
-                        "Lupa password?",
-                        style: TextStyle(
-                          color: Color(0xff656260),
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xff25DB91),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(999),
-                      )),
-                  child: const Text(
-                    "LOGIN",
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff25DB91),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(999),
+                    )),
+                child: const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    fontSize: 15,
                   ),
                 ),
-                const SizedBox(height: 0.00001),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Belum punya akun?"),
-                    TextButton(
-                      onPressed: () {
-                        Get.toNamed(Routes.REGISTER);
-                      },
-                      child: const Text(
-                        "Daftar sekarang",
-                        style: TextStyle(
-                          color: Color(0xff25DB91),
-                          decoration: TextDecoration.underline,
-                        ),
+              ),
+              const SizedBox(height: 0.00001),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Belum punya akun?"),
+                  TextButton(
+                    onPressed: () {
+                      Get.toNamed(Routes.REGISTER);
+                    },
+                    child: const Text(
+                      "Daftar sekarang",
+                      style: TextStyle(
+                        color: Color(0xff25DB91),
+                        decoration: TextDecoration.underline,
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
